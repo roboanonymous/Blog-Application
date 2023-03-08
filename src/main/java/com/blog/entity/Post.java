908ab129c9a7b6d219517e9e213e317458801026
roboanonymous <jsamyak591@gmail.com>
2023-03-08 15:38:12 +0530

    first

--- src/main/java/com/blog/entity/Post.java
+++ src/main/java/com/blog/entity/Post.java
@@ -1,5 +1,40 @@
 package com.blog.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Post")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Post {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Integer postId;
+	
+	@Column(name = "Post_title" , nullable = false, length = 100)
+	 private String title;
+	 private String Content;
+	 private String imageName;
+	 private Date addeddate;
+	 
+	 @ManyToOne
+	 private Category category;
+	 
+	 @ManyToOne
+	 private User1 user1;
+	 
+	 
 }
--- src/main/java/com/blog/entity/User1.java
+++ src/main/java/com/blog/entity/User1.java
@@ -1,10 +1,16 @@
 package com.blog.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +32,8 @@ public class User1 {
 	private String password;
 	private String about;
 	
+	@OneToMany(mappedBy = "user1" , cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 	
 
 }
